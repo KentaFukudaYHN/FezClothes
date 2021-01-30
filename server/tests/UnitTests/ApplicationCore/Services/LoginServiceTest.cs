8a97edea75caefba0fe734d239fd30062c99db8e
Kenta Fukuda <yhn_18782@icloud.com>
2021-01-31 06:42:27 +0900

    テストプロジェクトの作成

---
+++ server/tests/UnitTests/ApplicationCore/Services/LoginServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.ApplicationCore.Services
+{
+    /// <summary>
+    /// ログインServiceテスト
+    /// </summary>
+    public class LoginServiceTest
+    {
+        /// <summary>
+        /// ログインテスト
+        /// </summary>
+        [Fact]
+        public void Login()
+        {
+        }
+    }
+}
