0268a579879558998cb692672f7b38e62a07188a
Kenta Fukuda <yhn_18782@icloud.com>
2021-02-02 16:40:07 +0900

    App.vueの仮実装

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>Hello</p>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Emit, Watch } from "vue-property-decorator";
+
+@Component({})
+export default class App extends Vue{
+    
+}
+</script>
